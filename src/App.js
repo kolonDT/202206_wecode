e148fd5e9edfc8ab6d7a8924cffed8835443e194
shw779 <shw779@naver.com>
2022-05-18 00:01:48 +0900

    ADD: 요청내역 페이지 기능구현 및 반응형 구현

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import "./App.css";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Login from "./Pages/Login/Login";
+import CompleteForm from "./Pages/Complete/CompleteForm";
 
 function App() {
   return (
-  <>
-  <Header/>
-  <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Login />} />
-      {/* <Route path='/' element={}/>
-      <Route path='/' element={}/>
-      <Route path='/sellcar' element={}/>
-      <Route path='/confirm' element={}/>
-      <Route path='/complete' element={}/>
-      <Route path='/requestform' element={}/>
-      <Route path='/admin' element={}/> */}
-    </Routes>
-  </BrowserRouter>
- <Footer/>
- </>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/complete" element={<CompleteForm />} />
+          {/* <Route path='/' element={}/>
+              <Route path='/' element={}/>
+              <Route path='/sellcar' element={}/>
+              <Route path='/confirm' element={}/>
+              <Route path='/requestform' element={}/>
+              <Route path='/admin' element={}/> */}
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
