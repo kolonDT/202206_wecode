be904b45aca116b0232b880933312e68bd1a4e94
최정민 <mango@choejeongmin-ui-MacBookPro.local>
2022-06-22 17:11:29 +0900

    fix

--- src/config.js
+++ src/config.js
@@ -3,9 +3,6 @@
 // export const PORT = process.env.REACT_APP_NODE_API_PORT;
 // export const URL = process.env.REACT_APP_NODE_API_URL;
 
-export const PORT = "9989"
-export const URL = "http://10.133.30.30"
-
 
 export const URI = `${URL}:${PORT}`;
 
